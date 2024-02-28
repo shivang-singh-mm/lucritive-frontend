92246665c810a52fa5e3cd0c4a4a4fd3c4b7c814
Shivang Singh <shivangsingh2240@gmail.com>
2024-02-28 20:41:00 +0530

    frontend

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,9 @@
+export const host = process.env.BACKEND_URL 
+// || "http://localhost:5000";
+export const loginRoute = `https://lucritive-app.adaptable.app/api/auth/login`;
+export const registerRoute = `https://lucritive-app.adaptable.app/api/auth/register`;
+export const logoutRoute = `https://lucritive-app.adaptable.app/api/auth/logout`;
+export const allUsersRoute = `https://lucritive-app.adaptable.app/api/auth/allusers`;
+export const sendMessageRoute = `https://lucritive-app.adaptable.app/api/messages/addmsg`;
+export const recieveMessageRoute = `https://lucritive-app.adaptable.app/api/messages/getmsg`;
+export const setAvatarRoute = `https://lucritive-app.adaptable.app/api/auth/setavatar`;
